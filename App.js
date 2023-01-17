7a2f776c9cfd563cf5b75f6ad141e16d306643e1
hennabackman <henna.backman@gmail.com>
2023-01-17 14:34:57 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, FlatList } from 'react-native';
 
-export default function App() {
+const Calculator = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  const [calculations, setCalculations] = useState([]);
+
+  const handleAddition = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+    setCalculations([...calculations, { calculation: `${num1} + ${num2} = ${sum}` }]);
+  }
+
+  const handleSubtraction = () => {
+    const difference = parseFloat(num1) - parseFloat(num2);
+    setResult(difference);
+    setCalculations([...calculations, { calculation: `${num1} - ${num2} = ${difference}` }]);
+  }
+  const HistoryHeader = () => {
+    return (
+        <Text style={styles.headerText}>History</Text>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.resultContainer}>
+        <Text 
+        style={[styles.resultText, {marginTop: 200}]}>{"Result: "+result}
+        </Text>
+      </View>
+      <TextInput
+        keyboardType='numeric'
+        value={num1}
+        onChangeText={text => setNum1(text)}
+        placeholder="Enter first number"
+        style={styles.input}
+      />
+      <TextInput
+        keyboardType='numeric'
+        value={num2}
+        onChangeText={text => setNum2(text)}
+        placeholder="Enter second number"
+        style={styles.input}
+      />
+      <View style={styles.buttonContainer}>
+      <Button
+      title='+'
+      onPress={handleAddition}
+      style={styles.button}
+      />
+      <Button
+      title='-'
+      onPress={handleSubtraction}
+      style={styles.button}
+      />
+      </View>
+      <FlatList
+      ListHeaderComponent={<HistoryHeader />}
+      data={calculations}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item }) => (
+      <Text style={styles.calculationText}>{item.calculation}</Text>
+  )}
+/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    padding: 10,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '80%',
+    alignSelf: 'center'
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 10
+  },
+  button: {
+    margin: 10,
+    width: '50%',
+    alignSelf: 'center'
+  },
+  resultContainer: {
+    alignSelf: 'center',
+    marginTop: 20
+  },
+  resultText: {
+    fontSize: 20,
+    alignSelf: 'center'
+  },
+  calculationText: {
+    fontSize: 16,
+    alignSelf: 'center'
+  },
+  headerText: {
+    margin: 20,
+    fontSize: 16,
+    alignSelf: 'center'
+  },
+
+
 });
+
+export default Calculator;
